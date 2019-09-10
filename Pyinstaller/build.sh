b7091d8712d5897762f902cb03747bcb82a67885
Gabriele Iannetti <g.iannetti@gsi.de>
2019-09-10 10:12:28 +0200

    add build script for Pyinstaller

---
+++ Pyinstaller/build.sh
@@ -0,0 +1,90 @@
+#!/bin/bash
+# -*- coding: utf-8 -*-
+#
+# Copyright 2019 Gabriele Iannetti <g.iannetti@gsi.de>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+
+
+TARGET_DIR=pybuild
+SOURCE_DIR=../../
+
+# Use pyinstaller default paths:
+# --distpath=.dist
+# --workpath=.build
+
+LUSTRE_GROUP_QUOTA_COLLECT_EXE=lustre-group-quota-collect.py
+LUSTRE_WEEKLY_REPORTS_EXE=lustre-weekly-reports.py
+LUSTRE_MONTHLY_REPORTS_EXE=lustre-monthly-reports.py
+LUSTRE_MIGRATION_REPORT_EXE=lustre-migration-report.py
+
+
+# $1 = expects executable file
+function build() {
+
+    if [ "$#" -ne 1 ]; then
+        echo "Build function expects executable file!"
+        exit 2
+    fi
+
+    EXE=$1
+
+    mkdir -p "$TARGET_DIR"
+    cd "$TARGET_DIR"
+
+    $(pyinstaller --onefile --name ${EXE} $SOURCE_DIR/${EXE})
+
+    if [ -f "dist/${EXE}" ]; then
+        echo ">>> SUCCESSFUL BUILD: dist/${EXE}"
+    else
+        echo ">>> FAILED BUILD: dist/${EXE}"
+    fi
+
+    cd - 1>/dev/null
+
+}
+
+case "$1" in
+
+    all)
+
+        build ${LUSTRE_GROUP_QUOTA_COLLECT_EXE}
+        build ${LUSTRE_WEEKLY_REPORTS_EXE}
+        build ${LUSTRE_MONTHLY_REPORTS_EXE}
+        build ${LUSTRE_MIGRATION_REPORT_EXE}
+    ;;
+
+    quota_collect)
+        build ${LUSTRE_GROUP_QUOTA_COLLECT_EXE}
+    ;;
+
+    weekly_reports)
+        build ${LUSTRE_WEEKLY_REPORTS_EXE}
+    ;;
+
+    monthly_reports)
+        build ${LUSTRE_MONTHLY_REPORTS_EXE}
+    ;;
+
+    migration_report)
+        build ${LUSTRE_MIGRATION_REPORT_EXE}
+    ;;
+
+    *)
+        echo "Usage: $0 {all|quota_collect|weekly_reports|monthly_reports|migration_report}"
+        exit 1
+    ;;
+
+esac
